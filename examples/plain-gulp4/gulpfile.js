e49c5cfcefa658bf5b37caa8970dd949cef5cac0
Chris Montgomery <chr.montgomery@gmail.com>
2015-09-16 22:07:07 -0500

    get the basics working with a typical gulpfile

---
+++ examples/plain-gulp4/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('../../index.js')(require('gulp'));
+
+// --------------------------------------------------------------------------------------
+// tasks
+// --------------------------------------------------------------------------------------
+
+function clean(done) {
+  console.log('cleaning...');
+  done();
+}
+clean.description = "clean it!!!";
+
+function coffee(done) {
+  console.log('coffeeing...');
+  done();
+}
+coffee.description = "This is a super duper long message that probably wraps and i want to see what it looks like when it does";
+
+function uglify(done) {
+  console.log('uglifying...');
+  done();
+}
+uglify.description = 'Uglify the things';
+
+function sass(done) {
+  console.log('sassing...');
+  done();
+}
+
+var lint = function (done) {
+  console.log('linting...');
+  done();
+};
+
+var build = gulp.series(
+  clean,
+  gulp.parallel(sass, gulp.series(coffee, uglify))
+);
+build.description = "this builds all the things";
+
+gulp.task('build', build);
+
+gulp.task('clean', clean);
+
+gulp.task(coffee);
+
+gulp.task('sassy', sass);
+
+gulp.task(uglify);
+
+gulp.task('lint', lint);
+
+gulp.task('a-super-long-task-name-to-see-margins', function (done) {
+  done();
+});
--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 var objectAssign = require('object-assign'),
   chalk = require('chalk'),
+  DEFAULT_HELP_TEXT = 'Display this help text.',
   DEFAULT_OPTIONS = {};
 
 module.exports = function (gulp, options) {
-  var originalTaskFn = gulp.task;
+  //var originalTaskFn = gulp.task;
 
-  options = objectAssign({}, DEFAULT_OPTIONS, options);
+  //options = objectAssign({}, DEFAULT_OPTIONS, options);
 
-  gulp.task = function (name, description, fn) {
+  //gulp.task = function (name, description, fn) {
+  //
+  //};
 
-  };
+  function help(done) {
+    logTasks(gulp);
+    done();
+  }
+  help.description = DEFAULT_HELP_TEXT;
+
+  gulp.task('help', help);
+
+  // do not add default task if one already exists
+  if (gulp._registry._tasks['default'] === undefined) {
+    gulp.task('default', gulp.series('help'));
+  }
 
   return gulp;
 };
+
+function logTasks(localGulp) {
+  var tasks = localGulp._registry._tasks,
+    margin = Object.keys(tasks).reduce(function (m, taskName) {
+      if (m > taskName.length) {
+        return m;
+      }
+      return taskName.length;
+    }, 0);
+
+  console.log('\n' + chalk.underline('Available tasks'));
+
+  Object.keys(tasks).forEach(function (taskName) {
+
+    var description = tasks[taskName].description || '';
+    var args = [' ', chalk.cyan(taskName)];
+
+    args.push(new Array(margin - taskName.length).join(' '));
+    args.push(description);
+
+    console.log.apply(console, args);
+  });
+
+  console.log('');
+}
