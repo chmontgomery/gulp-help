7e1f62a1f846c57afa2f47b5852d54cb69587656
Kat <katharine.a.maclean@gmail.com>
2015-12-05 13:35:41 +0000

    London theme

---
+++ themes/london.js
@@ -0,0 +1,13 @@
+var chalk = require('chalk');
+
+module.exports = {
+  styles: {
+  	taskLinePrefix: null,
+    taskName: chalk.bold.blue,
+    tasksHeading: chalk.bold.underline.black,
+    description: chalk.grey,
+    flagName: chalk.red,
+    flagPrefix: null,
+    flagDescription: null
+  }
+};
