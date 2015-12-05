ac063c496e81a691760a728c8604b84cdc63ee1d
Amelia Whitehouse <ameliawhitehouse@Amelias-MacBook-Air.local>
2015-12-05 13:35:42 +0000

    unicorn theme

---
+++ themes/unicorn.js
@@ -0,0 +1,13 @@
+var chalk = require('chalk');
+
+module.exports = {
+  styles: {
+  	taskLinePrefix: null,
+    taskName: chalk.bold.magenta,
+    tasksHeading: chalk.bold.underline.black,
+    description: chalk.grey,
+    flagName: chalk.italic.blue,
+    flagPrefix: null,
+    flagDescription: null
+  }
+};
